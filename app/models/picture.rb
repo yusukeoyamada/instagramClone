009bd9b78a087d54c9b12aa8e048f0ea0a0fe640
yusukeoyamada <fa19940118@gmail.com>
2018-05-13 22:11:38 +0900

    投稿画面の画像にバリデーション追加

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,7 +1,8 @@
 class Picture < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true, length: { in: 1..140 }
-
+  validates :image, presence: true
+  
   belongs_to :user
   has_many :favorites, dependent: :destroy
   has_many :favorite_users, through: :favorites, source: :user
